191ad05df7f22c24d1b9aff2c42001fa0229a879
Leandro Coelho <leandrocdm@live.com>
2019-09-21 11:46:18 -0300

    finalizando curso java parte 2

--- Java parte 2 - Introducao a orientacao a Objetos/workspace/Cyberbank/src/Conta.java
+++ Java parte 2 - Introducao a orientacao a Objetos/workspace/Cyberbank/src/Conta.java
@@ -1,10 +1,19 @@
 public class Conta {
 	
-	double saldo;
-	int agencia;
-	int numero;
-	Cliente titular;
-	char codigo;
+	private double saldo;
+	private int agencia;
+	private int numero;
+	private Cliente titular;
+	private char codigo;
+	private static int total;
+	
+	public Conta() { Conta.total++; }
+	
+	public Conta(int agencia, int numero) {
+		this.agencia = agencia;
+		this.numero = numero;
+		Conta.total++;
+	}
 	
 	public void depositar(double valor) {
 		this.saldo += valor;
@@ -27,5 +36,49 @@ public class Conta {
 			destino.saldo += valor;
 			return true;
 		}
+	}
+	
+	public double getSaldo() {
+		return this.saldo;
+	}
+	
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	
+	public int getAgencia() {
+		return this.agencia;
+	}
+	
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
+	
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	
+	public int getNumero() {
+		return numero;
+	}
+	
+	public Cliente getTitular() {
+		return titular;
+	}
+	
+	public void setTitular(Cliente titular) {
+		this.titular = titular;
+	}
+	
+	public char getCodigo() {
+		return codigo;
+	}
+	
+	public void setCodigo(char codigo) {
+		this.codigo = codigo;
+	}
+	
+	public static int getTotal() {
+		return Conta.total;
 	}
 }
--- Java parte 2 - Introducao a orientacao a Objetos/workspace/Cyberbank/src/TestaContaCliente.java
+++ Java parte 2 - Introducao a orientacao a Objetos/workspace/Cyberbank/src/TestaContaCliente.java
@@ -4,13 +4,22 @@ public class TestaContaCliente {
 		
 		Cliente cliente = new Cliente();
 		
-		cliente.nome = "Leandro";
-		
 		Conta conta = new Conta();
 		
-		conta.depositar(100);
-		conta.titular = cliente;
+		cliente.setNome("Leandro");
+		cliente.setCpf("1231231211");
+		cliente.setProfissao("Engenheiro");
 		
-		System.out.println(conta.titular.nome);
+		conta.depositar(100);
+		
+		conta.setTitular(cliente);
+		
+		System.out.println(conta.getSaldo());
+		
+		System.out.println(conta.getTitular().getNome());
+		
+		System.out.println(Conta.getTotal());
+		
 	}
 }
+ 
