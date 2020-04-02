2c0cd27632ef287c9e30de19f2f76b15d07f0069
Leandro Coelho de Menezes <leandrocdm@MacBook-Pro-de-Leandro.local>
2020-04-01 21:18:24 -0300

    removendo projeto

--- controle-estoque/src/Console.java
+++
@@ -1,9 +0,0 @@
-public class Console {
-	
-	public static void main(String args[]) {
-		
-		Menu menu = new Menu();
-		menu.menuOpcoes(menu.menuPrincipal());
-	}
-
-}
--- controle-estoque/src/GerarNotaFiscal.java
+++
@@ -1,5 +0,0 @@
-public interface GerarNotaFiscal {
-	
-	void enviarCpfOuCnpj(String cpfOuCnpj);
-
-}
